4c84079c0091b85789a3dff7c220728c1538f56d
bazbt3 <bazbt3@users.noreply.github.com>
2021-05-11 19:31:42 +0100

    Added rudimentary combat (random numbers weighted towards grue)

--- main.ts
+++ main.ts
@@ -1,84 +1,81 @@
-controller.up.onEvent(ControllerButtonEvent.Pressed, function on_up_pressed() {
-    
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     if (roomdirections[y][x].includes("N")) {
         y = y - 1
         boundarycheck()
     } else {
         noexit()
     }
-    
-    mySprite.setImage(assets.image`
-        up
-    `)
+    mySprite.setImage(assets.image`up`)
     mySprite.say("You can go " + roomdirections[y][x])
 })
-controller.left.onEvent(ControllerButtonEvent.Pressed, function on_left_pressed() {
-    
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     if (roomdirections[y][x].includes("W")) {
         x = x - 1
         boundarycheck()
     } else {
         noexit()
     }
-    
-    mySprite.setImage(assets.image`
-        left
-    `)
+    mySprite.setImage(assets.image`left`)
     mySprite.say("You can go " + roomdirections[y][x])
 })
-function showstate() {
-    
+function showstate () {
     if (x == 0 && y == 2) {
         successtext = "completed"
     }
-    
     game.splash("There is " + roomcontents[y][x])
+    if (roomcontents[y][x].includes("grue")) {
+        combat()
+    }
     if (successtext != "") {
         music.magicWand.play()
-        mySprite.setImage(assets.image`
-            smiley
-        `)
+        mySprite.setImage(assets.image`smiley`)
         pause(2000)
         game.over(true, effects.confetti)
     }
-    
 }
-
-controller.right.onEvent(ControllerButtonEvent.Pressed, function on_right_pressed() {
-    
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     if (roomdirections[y][x].includes("E")) {
         x = x + 1
         boundarycheck()
     } else {
         noexit()
     }
-    
-    mySprite.setImage(assets.image`
-        right
-    `)
+    mySprite.setImage(assets.image`right`)
     mySprite.say("You can go " + roomdirections[y][x])
 })
-controller.down.onEvent(ControllerButtonEvent.Pressed, function on_down_pressed() {
-    
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     if (roomdirections[y][x].includes("S")) {
         y = y + 1
         boundarycheck()
     } else {
         noexit()
     }
-    
-    mySprite.setImage(assets.image`
-        down
-    `)
+    mySprite.setImage(assets.image`down`)
     mySprite.say("You can go " + roomdirections[y][x])
 })
-function noexit() {
+function combat () {
+    music.siren.play()
+    mySprite.say("'B' starts combat!")
+    game.splash("You must do combat with " + roomcontents[y][x] + "!")
+    combatadversary = randint(0, 7)
+    combatplayer = randint(0, 6)
+    while (!(controller.B.isPressed())) {
+        pause(100)
+    }
+    mySprite.say("You fight!")
+    if (combatadversary >= combatplayer) {
+        game.splash("Beaten by " + roomcontents[y][x] + "!")
+        game.over(false)
+    } else if (combatadversary < combatplayer) {
+        music.baDing.play()
+        game.splash("You beat ", "" + roomcontents[y][x] + "!!")
+    }
+}
+function noexit () {
     music.buzzer.play()
     game.splash("No exit that way.", "You can go " + roomdirections[y][x])
 }
-
-function boundarycheck() {
-    
+function boundarycheck () {
     if (x == -1) {
         x = 0
     } else if (x == 3) {
@@ -88,25 +85,23 @@ function boundarycheck() {
     } else if (y == 3) {
         y = 2
     }
-    
     showstate()
 }
-
-let mySprite : Sprite = null
+let combatplayer = 0
+let combatadversary = 0
+let mySprite: Sprite = null
 let successtext = ""
 let y = 0
 let x = 0
-let roomcontents : string[][] = []
-let roomdirections : string[][] = []
+let roomcontents: string[][] = []
+let roomdirections: string[][] = []
 roomdirections = [["S", "ES", "SW"], ["NE", "NW", "NS"], ["E", "EW", "NW"]]
 roomcontents = [["an empty room here.", "another empty room.", "nothing here."], ["a cat here.", "a dog here.", "a rabbit here."], ["an end to this, your final objective!", "a grue!", "an empty room."]]
 x = 0
 y = 0
 successtext = ""
 game.showLongText("ADVENTURE!    " + "To move, press button A then a direction button.     " + ("There is " + roomcontents[y][x]), DialogLayout.Center)
-mySprite = sprites.create(assets.image`
-    static
-`, SpriteKind.Player)
+mySprite = sprites.create(assets.image`static`, SpriteKind.Player)
 mySprite.setStayInScreen(true)
 mySprite.setPosition(scene.screenWidth() / 2 - 40, scene.screenHeight() - 10)
 mySprite.say("You can go " + roomdirections[y][x])
